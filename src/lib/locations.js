3575b3a0cd0bd94c4d08c4d09cc42197736dcb48
Ariel Favaro <arieltainan@gmail.com>
2022-06-17 09:37:30 -0300

    removed old stuff

--- src/lib/locations.js
+++ src/lib/locations.js
@@ -1,18 +1,5 @@
 import addresses from './addresses';
 
-// for old usage...
-// const locations = Object.keys(addresses).map(key => {
-//     return {
-//         slug: key,
-//         lat: parseFloat(addresses[key].latitude),
-//         lng: parseFloat(addresses[key].longitude),
-//         nome: addresses[key].nome,
-//         endereco: addresses[key].endereco,
-//         telefone: addresses[key]?.telefone,
-//         cep: addresses[key]?.cep,
-//     };
-// });
-
 const locations = addresses.map(address => {
     return {
         slug: address.slug,
@@ -24,11 +11,5 @@ const locations = addresses.map(address => {
         cep: address.cep,
     };
 });
-
-// const locations = [
-//     { lat: -21.8517626, lng: -47.4904707 },
-//     { lat: -21.8537267, lng: -47.4887294 },
-//     { lat: -21.8573694, lng: -47.492614 },
-// ];
 
 export { locations }
